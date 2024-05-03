57b852c1151ac1bda4d52b744a6c150f652da09e
Raghav Agrawal <raghavagrawal01001@gmail.com>
2024-05-04 00:36:26 +0530

    Patient Doctor Profile

---
+++ backend/public/js/Patient/doctor_profile.js
@@ -0,0 +1,23 @@
+const patient_data = (id) => {
+  fetch("/authDoctor/users", {
+    method: "POST",
+    body: JSON.stringify({
+      id: id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("data: ", data);
+      document.getElementById("docName").innerHTML = "Dr. " + data.result.name;
+      document.getElementById("speciality").innerHTML = data.result.specialty;
+      
+    });
+};
+
+const getData = (id) => {
+  console.log("Patient Id: ", id);
+  patient_data(id);
+};
