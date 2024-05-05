32058f29972c2a130af04bc910e046912d8c52f4
Raghav Agrawal <raghavagrawal01001@gmail.com>
2024-05-05 14:19:05 +0530

    profile work done

--- backend/public/js/Doctor/doctor_profile_settings.js
+++ backend/public/js/Doctor/doctor_profile_settings.js
@@ -44,11 +44,10 @@ const getData = (id) => {
 // Data Updating for page starts here
 
 const update_doctor_profile = (id) => {
-  const doctor_name = document.getElementById("doctor_name").value;
+  const doctor_name = document.getElementById("doctor_name_form").value;
   const doctor_degree = document.getElementById("doctor_degree").value;
   const doctor_username = document.getElementById("doctor_username").value;
   const doctor_email = document.getElementById("doctor_email").value;
-  const doctor_name_form = document.getElementById("doctor_name_form").value;
   const doctor_phone_number = document.getElementById("doctor_phone_number").value;
   const doctor_address_form = document.getElementById("doctor_address_form").value;
   const gender_form = document.getElementById("gender_form").value;
@@ -56,20 +55,39 @@ const update_doctor_profile = (id) => {
   const about_me = document.getElementById("about_me").value;
   const services = document.getElementById("services").value.split(",");
   const specialist = document.getElementById("specialist").value.split(",");
-  console.log("Update Files: ", 
-    "\ndoctor_name",doctor_name,
-    "\ndoctor_degree",doctor_degree,
-    "\ndoctor_username",doctor_username,
-    "\ndoctor_email",doctor_email,
-    "\ndoctor_name_form",doctor_name_form,
-    "\ndoctor_phone_number",doctor_phone_number,
-    "\ndoctor_address_form",doctor_address_form,
-    "\ngender_form",gender_form,
-    "\ndoctor_dob_form",doctor_dob_form,
-    "\nabout_me",about_me,
-    "\nservices",services,
-    "\nspecialist",specialist,
-  )
+
+  const degree = document.querySelectorAll(".degree_class");
+  const institute = document.querySelectorAll(".institute_class");
+  const year_of_completion = document.querySelectorAll(".year_of_completion_class");
+  let education = [];
+  
+  for (let i = 0; i < degree.length; i++) {
+    const edu = {}; // Create an empty object first
+
+    // Check conditions and set properties only if necessary
+    if ( degree[i].value.length > 0 && institute[i].value.length > 0 && year_of_completion[i].value.length > 0) {
+      edu.degree = degree[i].value;
+      edu.institute = institute[i].value;
+      edu.year_of_completion = year_of_completion[i].value;
+    }
+    education.push(edu);
+  }
+
+  const data = {
+    name: doctor_name,
+    email: doctor_email,
+    contact: doctor_phone_number,
+    address: doctor_address_form,
+    gender: gender_form,
+    DOB: doctor_dob_form,
+    degree: doctor_degree,
+    about_me: about_me,
+    // pricing: doctor_pricing,
+    services: services,
+    Specialization: specialist,
+  };
+
+  console.log(data);
 
 }
 
